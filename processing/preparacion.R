a0e7b76ddd5bc7433ad6ce234c53de554a7047fc
mafenunez <146782034+mafenunez@users.noreply.github.com>
2025-07-20 17:24:13 -0400

    avances

--- processing/preparacion.R
+++ processing/preparacion.R
@@ -1,42 +1,75 @@
+# 0. PREPARACIÓN DATOS OLA 2023
+# María Fernanda Núñez y Tomás Urzúa
 
-pacman::p_load(tidyverse, dplyr, car, haven)
+# ---- 1. cargar librerias ----------------------------------------------------
+pacman::p_load(tidyverse, dplyr, car)
 
-proc_data <- read_sav("input/ELSOC_Long_2016_2023.sav")
-proc_data <- proc_data %>% select(just_educacion = d02_02,
-                                              cercania_escuelas = t06_06,
-                                              remuneracion_justa = m15,
-                                               ingreso_hogar = m29,
-                                               estatus_subj = d01_01,
-                                                genero = m0_sexo,
-                                                  ola) %>% filter(ola == 7)
+# ---- 2. cargar datos --------------------------------------------------------
+load("input/elsoc_2023.RData")
 
-## ingreso
+# ---- 3. seleccionar ---------------------------------------------------------
+proc_data <- elsoc_2023 %>% select(just_educacion = d02_02,
+                                   ingreso_hogar = m29,
+                                   estatus_subj = d01_01,
+                                   genero = m0_sexo,
+                                   esfuerzo = c18_09,
+                                   talento = c18_10) 
+
+# ---- 4. procesamiento -------------------------------------------------------
+
+## ingreso:
 proc_data$ingreso_hogar <-recode(proc_data$ingreso_hogar, "c(-777,-888,-999)=NA")
 
-## ingreso justo
-proc_data$remuneracion_justa <-recode(proc_data$remuneracion_justa, "c(-888,-999)=NA")
 
-
-## estatus social subjetivo
+## estatus social subjetivo:
 proc_data$estatus_subj <-recode(proc_data$estatus_subj, "c(-666,-777,-888,-999)=NA")
 proc_data$estatus_subj <- as.numeric(proc_data$estatus_subj)
 
-## proximidad colegios
-proc_data$cercania_escuelas <- as.factor(proc_data$cercania_escuelas)
-proc_data$cercania_escuelas <- car::recode(proc_data$cercania_escuelas,
-                               recodes = c("1 = ' Totalmente insatisfecho';
-                                                       2 = 'Insatisfecho ';
-                                                       3 = 'Ni satisfecho ni insatisfecho';
-                                                       4 = 'Satisfecho ';
-                                                       5 = 'Totalmente satisfecho';
+                            
+## esfuerzo
+proc_data$esfuerzo <- as.factor(proc_data$esfuerzo)
+proc_data$esfuerzo <- car::recode(proc_data$esfuerzo,
+                                        recodes = c("1 = 'Totalmente en desacuerdo';
+                                                       2 = 'En desacuerdo';
+                                                       3 = 'Ni de acuerdo ni en desacuerdo';
+                                                       4 = 'De acuerdo';
+                                                       5 = 'Totalmente de acuerdo';
                                                        -888 = NA; 
                                                        -999 = NA;
                                                       -777 = NA;
-                                                      -666=NA"))
+                                                      -666=NA"),
+                                  levels = c("Totalmente en desacuerdo",
+                                             "En desacuerdo",
+                                             "Ni de acuerdo ni en desacuerdo",
+                                             "De acuerdo",
+                                             "Totalmente de acuerdo"))
 
+## talento 
+proc_data$talento <- as.factor(proc_data$talento)
+proc_data$talento <- car::recode(proc_data$talento,
+                                        recodes = c("1 = 'Totalmente en desacuerdo';
+                                                       2 = 'En desacuerdo';
+                                                       3 = 'Ni de acuerdo ni en desacuerdo';
+                                                       4 = 'De acuerdo';
+                                                       5 = 'Totalmente de acuerdo';
+                                                       -888 = NA; 
+                                                       -999 = NA;
+                                                      -777 = NA;
+                                                      -666=NA"),
+                                 levels = c("Totalmente en desacuerdo",
+                                            "En desacuerdo",
+                                            "Ni de acuerdo ni en desacuerdo",
+                                            "De acuerdo",
+                                            "Totalmente de acuerdo"))
 
+## género
 
-## dependiente
+proc_data$genero <- as.factor(proc_data$genero)
+proc_data$genero <- car::recode(proc_data$genero,
+                                recodes = c("1 = 'Hombre';
+                                                       2 = 'Mujer'"))
+
+## justicia de mercado en la educación
 
 proc_data$just_educacion <- as.factor(proc_data$just_educacion)
 proc_data$just_educacion <- car::recode(proc_data$just_educacion,
@@ -48,14 +81,18 @@ proc_data$just_educacion <- car::recode(proc_data$just_educacion,
                                                        -888 = NA; 
                                                        -999 = NA;
                                                       -777 = NA;
-                                                      -666=NA"))
+                                                      -666=NA"),
+                               levels = c("Totalmente en desacuerdo",
+                                          "En desacuerdo",
+                                          "Ni de acuerdo ni en desacuerdo",
+                                          "De acuerdo",
+                                          "Totalmente de acuerdo"))
 
 
-proc_data$genero <- as.factor(proc_data$genero)
-proc_data$genero <- car::recode(proc_data$genero,
-                                        recodes = c("1 = 'Hombre';
-                                                       2 = 'Mujer'"))
-# ---- 4. guardar datos procesados --------------------------------------------
+## tratamiento casos perdidos:
+proc_data <- proc_data %>% na.omit() # se omiten 260 casos, un 9,53% de la muestra
+
+# ---- 5. guardar datos procesados --------------------------------------------
 save(proc_data,file = "output/proc_data.RData")
 
 
